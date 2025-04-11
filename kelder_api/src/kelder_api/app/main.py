01fd4b00218789e65b5b27711661efc0fa7af04f
tomb2412 <tombramwell610@gmail.com>
2025-04-11 18:57:06 +0100

    began creating gps endpoint

--- kelder_api/src/kelder_api/app/main.py
+++ kelder_api/src/kelder_api/app/main.py
@@ -2,6 +2,12 @@ from typing import Union
 
 from fastapi import FastAPI
 
+import serial
+import time
+import string
+import pynmea2
+
+
 app = FastAPI()
 
 
@@ -13,4 +19,23 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/gps_coords")
+def get_gps_coords():
+    port="/dev/ttyAMA0"
+    ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+    dataout = pynmea2.NMEAStreamReader()
+    newdata=ser.readline()
+
+    if newdata[0:6] == "$GPRMC":
+        newmsg=pynmea2.parse(newdata)
+        lat=newmsg.latitude
+        lng=newmsg.longitude
+        gps = "Latitude=" + str(lat) + "and Longitude=" + str(lng)
+
+        return {"Latitude":  str(lat), "Longitude": str(lng)}
+    
+    else:
+        return {}
 
