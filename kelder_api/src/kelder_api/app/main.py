2bf1708c69505d2fdba7e221cb1c632299cf5cf2
tomb2412 <tombramwell610@gmail.com>
2025-04-13 09:02:49 +0100

    added log configuration

--- kelder_api/src/kelder_api/app/main.py
+++ kelder_api/src/kelder_api/app/main.py
@@ -1,5 +1,6 @@
 import logging
 from typing import Union
+from datetime import datetime
 
 from fastapi import FastAPI
 
@@ -7,7 +8,15 @@ from src.kelder_api.components.health.views import router as health_route
 from src.kelder_api.components.gps.views import router as gps_route
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename="logs/kelder_api.log")
+logging.basicConfig(
+    filename=f"logs/{datetime.now().strftime("%Y-%m-%d")}_kelder_api.log",
+    encoding = "utf-8",
+    filemode = "a",
+    format="{levelname} - {asctime} - {message}",
+    style = "{",
+    datefmt = "%Y-%m-%d %H:%M:%S",
+    level=logging.DEBUG
+)
 
 app = FastAPI()
 
