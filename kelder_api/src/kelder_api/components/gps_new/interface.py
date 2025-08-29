4b1ab05a8eece0938f3cd8b6a2650bfe1509053b
Tom Bramwell <tombramwell610@gmail.com>
2025-08-29 14:01:16 +0100

    Added new GPS interface

---
+++ kelder_api/src/kelder_api/components/gps_new/interface.py
@@ -0,0 +1,202 @@
+from typing import AsyncGenerator, Dict, List
+from contextlib import asynccontextmanager
+import serial_asyncio
+import asyncio
+import logging
+import pynmea2
+import serial
+from pynmea2 import NMEASentence
+from datetime import datetime
+
+from time import time
+
+from src.kelder_api.components.redis_client.redis_client import RedisClient
+from src.kelder_api.configuration.settings import Settings
+from src.kelder_api.components.gps_new.models import (
+    GPGSVSatellitesInView,
+    GPGSAActiveSatellites,
+    GPRMCRecommendedCourse,
+    GPSRedisData
+)
+
+logger = logging.getLogger(__name__)
+
+class GPSInterface:
+    """
+    Provides a GPS interface, through a serial connection with the sensor
+    """
+
+    def __init__(self, redis_client: RedisClient):
+        self.redis_client = redis_client
+
+        #Attributes for serial gps connection
+        self.PORT = Settings().gps.gps_serial_port
+        self.BAUDRATE = Settings().gps.gps_baudrate
+        self.TIMEOUT = Settings().gps.gps_timeout
+
+        #Attributes for parsing the serial data stream
+        self.sentence_models: Dict[str, callable] = {
+            'GPRMC': self._set_gprmc_sentence,
+            'GPGSA': self._set_gpgsa_sentence,
+            'GPGSV': self._set_gpgsv_sentence,
+        }
+
+    @asynccontextmanager
+    async def _get_serial_connection(self) -> AsyncGenerator[asyncio.StreamReader, None]:
+        reader = None
+        writer = None
+        
+        try:
+            reader, writer = await serial_asyncio.open_serial_connection(
+                url=self.PORT,
+                baudrate=self.BAUDRATE,
+                timeout=self.TIMEOUT 
+            )
+            yield reader
+
+        except serial.SerialException as error:
+            logger.error(
+                "Serial connection to the GPS or port cannot be established. Potentially due to containers with incorrect access. \n {error}"
+            )
+            raise
+        except OSError as error:
+            logger.error(f"OS error opening serial port {self.port}: {error}")
+            raise
+        
+        finally:
+            if writer and not writer.is_closing():
+                try:
+                    writer.close()
+                    await writer.is_closed()
+                    logger.debug(f"GPS serial connection closed on port {self.PORT}")
+
+                except Exception as error:
+                    logger.error(f"Failed to properly close the gps serial connection on port {self.PORT}")
+                    raise
+    
+    def _set_gprmc_sentence(self, nmea_data: NMEASentence):
+        self.gprmc_recommended_course = GPRMCRecommendedCourse.from_nmea(nmea_data)
+
+    def _set_gpgsa_sentence(self, nmea_data: NMEASentence):
+        self.gpgsa_active_satellites = GPGSAActiveSatellites.from_nmea(nmea_data)
+    
+    def _set_gpgsv_sentence(self, nmea_data: NMEASentence):
+        self.gpgsv_satellites_in_view.from_nmea(nmea_data)
+    
+    async def write_gps(
+            self,
+            gpgsv_satellites_in_view,
+            gprmc_recommended_course,
+            gpgsa_active_satellites
+        ) -> None:
+        gps_redis_data = GPSRedisData(
+            timestamp = gprmc_recommended_course.timestamp,
+            status = gprmc_recommended_course.status,
+            latitude_nmea = gprmc_recommended_course.latitude_nmea,
+            longitude_nmea = gprmc_recommended_course.longitude_nmea,
+            active_prn = gpgsa_active_satellites.satilite_prns,
+            hdop = gpgsa_active_satellites.hdop,
+            satellites_in_view = gpgsv_satellites_in_view.satellites
+        )
+        await self.redis_client.write_set("GPS", gps_redis_data)
+        logger.debug("Successfully written GPS reading.")
+        
+    async def read_gps_latest(self, active: bool = False) -> GPSRedisData:
+        """Retrieves the lastest gps measurement regardless of status"""
+        try:
+            if active:
+                return (await self.read_active_gps_measurements())[0]
+            else:
+                return GPSRedisData(**(await self.redis_client.read_set("GPS"))[0])
+        except IndexError:
+            logger.error("No GPS data available")
+            return None
+    
+    async def read_gps_history_length(self, length: int, active: bool = False) -> List[GPSRedisData]:
+        """Retrieves the lastest n gps measurement regardless of status"""
+        if active:
+            gps_history = await self.read_active_gps_measurements()
+        else:
+            gps_history = [GPSRedisData(**gps_measurement) for gps_measurement in await self.redis_client.read_set("GPS")]
+        
+        try:
+            return gps_history[0:length]
+        except IndexError:
+            logger.debug(f"GPS history shorter than length requested. Returning length: {len(gps_history)}")
+            return gps_history
+        
+    async def read_gps_history_time_series(self, start_datetime: datetime, end_datetime: datetime | None = None, active: bool = False) -> List[GPSRedisData]:
+        """Retrieves the gps measurement within a datetime range"""
+        if not end_datetime:
+            end_datetime = datetime.now()
+        
+        gps_time_series = await self.redis_client.read_set("GPS", [start_datetime, end_datetime])
+
+        if active:
+            return [gps_measurement for gps_measurement in gps_time_series if gps_measurement["status"] == "A"]
+        else:
+            return gps_time_series
+
+    async def read_gps_all_history(self, active: bool = False)-> List[GPSRedisData]:
+        """Returns all GPS history""" 
+        if active:
+            return await self.read_active_gps_measurements()
+        else:
+            return await self.redis_client.read_set("GPS")
+
+    async def read_active_gps_measurements(self) -> List[GPSRedisData]:
+        """Returns only active GPS measurements from all history"""
+        return [
+            GPSRedisData(**active_measurement) for active_measurement in await self.redis_client.read_set("GPS") if active_measurement["status"] == "A"]
+
+    def _parse_serial_gps_string(self, sentence) -> None:
+        data_type = sentence[1:6]
+        try:
+            self.sentence_models[data_type](
+                pynmea2.parse(sentence)
+                )
+        except KeyError as error:
+            logger.debug(f"Could not parse unsupported NMEA sentence: {data_type}")
+        except Exception as error:
+            logger.error(f"Failed to parse the nmea string:\nstring: {sentence}\nerror: {error}")
+            return None
+        
+                        
+    async def stream_serial_data(self, mock_sentence_stream: List[str] | None = None) -> None:
+        """Public method to open the serial connection, and processes the datastream until a gps measurement is identified"""
+
+        # Clear the satellites in view and other sentense data before reading the serial stream
+        self.gpgsv_satellites_in_view = GPGSVSatellitesInView()
+        self.gprmc_recommended_course = None
+        self.gpgsa_active_satellites = None
+
+        gps_data_identified = False
+
+        if mock_sentence_stream:
+            mock_stream_index = 0
+
+        start_time = time()
+        while not gps_data_identified:
+            if mock_sentence_stream:
+                try:
+                    newsentence = mock_sentence_stream[mock_stream_index]
+                    mock_stream_index += 1
+                except IndexError:
+                    mock_stream_index = 0
+            else:
+                async with self._get_serial_connection() as serial_reader:
+                    newsentence = await serial_reader.readline()
+                newsentence = (newsentence.decode("utf-8", errors="ignore").strip())
+
+            if newsentence.startswith("$") or "," in newsentence:
+                self._parse_serial_gps_string(newsentence)
+
+            # track sentences complete
+            if self.gprmc_recommended_course and self.gpgsa_active_satellites and self.gpgsv_satellites_in_view.all_messages_read:
+                gps_data_identified = True
+
+            elif (time() - start_time) > self.TIMEOUT:
+                logger.error("Timeout exceeded reading GPS serial stream.")
+                raise TimeoutError("Timeout exceeded reading GPS serial stream.")
+
+        await self.write_gps(self.gpgsv_satellites_in_view, self.gprmc_recommended_course, self.gpgsa_active_satellites)
---
+++ kelder_api/src/kelder_api/components/gps_new/models.py
@@ -0,0 +1,134 @@
+from pydantic import BaseModel, Field, computed_field
+from datetime import time, datetime
+from pynmea2 import NMEASentence
+from typing import List, Dict
+
+from src.kelder_api.components.gps_new.types import (
+    GPSStatus,
+    LatitudeHemisphere,
+    LongitudeHemisphere
+)
+
+
+class GPRMCRecommendedCourse(BaseModel):
+    """
+    Data model containing a compilation of GPS statistics from the GPS stream
+    GPRMC: Recommended Minimum Navigation Information
+
+    Requires minimum data from a no fix sentence.
+    """
+
+    timestamp: datetime = Field(description="Timestamp taken from the GPRMC sentence")
+    status: GPSStatus = Field(description="Status of GPS reading")
+    latitude_nmea: str | None = Field(
+        description='Latitude NMEA output: DDMM.MMMM ("Degrees, decimal minutes")',
+        default = None
+    )
+    longitude_nmea: str | None = Field(
+        description='Longitude NMEA output: DDDMM.MMMM ("Degrees, decimal minutes")',
+        default = None
+    )
+
+    @classmethod
+    def from_nmea(cls, nmea_data: NMEASentence) -> 'GPSSerialGPRMC':
+        return cls(
+            timestamp = datetime.combine(nmea_data.datestamp, nmea_data.timestamp),
+            status = GPSStatus.ACTIVE if nmea_data.status == "A" else GPSStatus.VOID,
+            latitude_nmea = nmea_data.lon,
+            latitude_hemisphere = LatitudeHemisphere.NORTH if nmea_data.lat_dir == 'N' else LatitudeHemisphere.SOUTH,
+            longitude_hemisphere = nmea_data.lon,
+            longitude_nmea = LongitudeHemisphere.WEST if nmea_data.lon_dir == "W" else LongitudeHemisphere.EAST
+        )
+    
+class GPGSAActiveSatellites(BaseModel):
+    """
+    Tracks data recieved from the Active Satilites nmea sentence
+    """
+    satilite_prns: List[int] = Field(description="PRN of satilites being used for fix")
+    satilite_count: int = Field()
+    hdop: float = Field(description = "Horizontal dilution of position")
+
+    @classmethod
+    def from_nmea(cls, nmea_data: NMEASentence) -> 'GPSSerialGPGSA':
+        satilite_prns = [getattr(nmea_data,sat_prn[1]) for sat_prn in nmea_data.fields if sat_prn[1].startswith('sv_id')]
+        satilite_prns = [satilite_prn for satilite_prn in satilite_prns if satilite_prn != '']     
+
+        
+        return cls(
+            satilite_prns = satilite_prns,
+            satilite_count = len(satilite_prns),
+            hdop = nmea_data.hdop
+        )
+
+class SatelliteInfomation(BaseModel):
+    """Individual satellite data from the satellites in view sentence"""
+    prn: int = Field(description="The PRN number identifying the satellite")
+    elevation: int = Field(description="The elevation in degrees (0-90) above the horizon")
+    azimuth: int = Field(description="Azimuth in degrees (0-359), relative to true north")
+    snr: float | None = Field(description="Signal to noise ratio (0-99 dB-Hz). Blank/None if not tracked ")
+
+
+class GPGSVSatellitesInView(BaseModel):
+    """
+    Tracks satellites in veiw data from the serial datastream
+    """
+    expected_satellites: int | None = Field(description="Total number of satellites in veiw", default = None)
+    satellites: Dict[int, SatelliteInfomation] = Field(description="The satellite information", default = {})
+
+    def add_satellite(self, prn: int, elevation: int, azimuth: int, snr: float | None):
+        """Adds of updates satellite information by the PRN key"""
+        self.satellites[prn] = SatelliteInfomation(
+            prn=prn,
+            elevation=elevation,
+            azimuth=azimuth,
+            snr=snr
+        )
+
+
+    def from_nmea(self, nmea_data: NMEASentence):
+        # Track the number of satellites expected in the stream
+        if not self.expected_satellites:
+            self.expected_satellites = nmea_data.num_sv_in_view
+
+        #Each GPGSV contains 4 satellites data
+        for i in range(4):
+            satellite_prn = getattr(nmea_data, f'sv_prn_num_{i+1}')
+            if satellite_prn:
+                snr = getattr(nmea_data, f'snr_{i+1}')
+                self.add_satellite(
+                    prn = int(satellite_prn),
+                    elevation = getattr(nmea_data, f'elevation_deg_{i+1}'),
+                    azimuth = getattr(nmea_data, f'azimuth_{i+1}'),
+                    snr = float(snr) if snr != "" else None
+                )
+                
+    @computed_field
+    @property
+    def all_messages_read(self) -> bool:
+        if self.expected_satellites:
+            return int(self.expected_satellites) == len(self.satellites)
+        else:
+            return False
+
+class GPSRedisData(BaseModel):
+    # Required fields
+    timestamp: datetime = Field(description="Timestamp taken from the GPRMC sentence")
+    status: GPSStatus = Field(description="Status of GPS reading from GPRMC data")
+
+    # Optional fields depending on status
+    # Active fix data
+    latitude_nmea: str = Field(
+        description='Latitude NMEA output: DDMM.MMMM ("Degrees, decimal minutes")',
+        default = ""
+    )
+    longitude_nmea: str = Field(
+        description='Longitude NMEA output: DDDMM.MMMM ("Degrees, decimal minutes")',
+        default = ""
+    )
+
+    # Active or void fix data
+    active_prn: List[int] = Field(description="PRN identifiers for the active satellites", default = [])
+    hdop: float | str = Field(description="Horizontal dilution of position", default = "")
+    
+    # Void fix data
+    satellites_in_view: Dict[int, SatelliteInfomation] = Field(description="The satellite information", default = {})
