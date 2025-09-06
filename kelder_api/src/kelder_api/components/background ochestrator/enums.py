01c7fa41e3beeb0af48aa5c3b137f2042d51241b
Tom Bramwell <tombramwell610@gmail.com>
2025-09-06 12:01:25 +0100

    deleted the old components

--- kelder_api/src/kelder_api/components/background ochestrator/enums.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class VesselState(Enum, str):
-    UNDERWAY = "underway"
-    STATIONARY = "stationary"
-
-    # Other states could include anchored, moored, home
--- kelder_api/src/kelder_api/components/compass/exceptions.py
+++
@@ -1,5 +0,0 @@
-class I2CConnectionFailure(Exception):
-    def __init__():
-        super().__init__(
-            "Connection to the I2C board has failed. Check board status light and wiring."
-        )
