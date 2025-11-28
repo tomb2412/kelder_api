951089f06067946fd835e673acb7c2158e24dc65
Tom Bramwell <tombramwell610@gmail.com>
2025-11-28 19:44:30 +0000

    DB manager model

---
+++ kelder_api/src/kelder_api/routes/db_manager/models.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, Field
+
+from src.kelder_api.components.db_manager.models import JourneyHistoryRecord
+
+class JourneyHistory(BaseModel):
+    journeys: list[JourneyHistoryRecord] = Field(
+        description="Journey data sent to the passage history ui."
+        )
+    limit: int = Field(description="The number of journeys total.")
--- kelder_api/src/kelder_api/routes/db_manager/views.py
+++ kelder_api/src/kelder_api/routes/db_manager/views.py
@@ -6,6 +6,7 @@ from fastapi import APIRouter, Depends, HTTPException, Request
 from src.kelder_api.app.getters import get_db_manager
 from src.kelder_api.components.db_manager.models import JourneyHistoryRecord
 from src.kelder_api.components.db_manager.service import DBManager
+from src.kelder_api.routes.db_manager.models import JourneyHistory
 
 logger = logging.getLogger("api.routes.db_manager")
 
@@ -16,17 +17,18 @@ def get_dependancy(request: Request) -> DBManager:
     return get_db_manager(request.app)
 
 
-@router.get("/journeys/latest", response_model=JourneyHistoryRecord)
+@router.get("/journeys/latest", response_model=JourneyHistory)
 async def get_latest_journey(
     db_manager: DBManager = Depends(get_dependancy),
-) -> JourneyHistoryRecord:
-    logger.info("Request received for latest journey history record")
-    latest_journey = db_manager.latest_trip()
-
+    limit: int | None = None,
+) -> JourneyHistory:
+    logger.info("Request received for journey history")
+    latest_journey = db_manager.list_trips()
+        
     if latest_journey is None:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND,
             detail="No journey history available",
         )
 
-    return latest_journey
+    return JourneyHistory(journeys=latest_journey[:limit], limit = len(latest_journey))
